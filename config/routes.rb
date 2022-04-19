97a48339b54dbd9cff9560dbe083440923cfc8a9
ryouto715 <93704808+ryouto715@users.noreply.github.com>
2022-04-19 16:21:42 +0000

    login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 end
